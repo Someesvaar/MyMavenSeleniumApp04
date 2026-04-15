f948daf68192fa9d31e406fb7f87bf82e4e2f664
Someesvaar <someesvaar.v@gmail.com>
2026-04-15 16:36:41 +0530

    Refactor main method for Selenium tests

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -14,74 +14,79 @@ import java.time.Duration;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
+        // Set ChromeDriver path (important for Jenkins)
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized"); // optional
+
+        // 🔥 MUST for Jenkins (headless environment)
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.manage().window().setSize(new org.openqa.selenium.Dimension(1920, 1080));
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
         Actions actions = new Actions(driver);
 
-        driver.get("https://www.saucedemo.com/");
+        try {
+            // ---------- Test 1 ----------
+            driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")))
+                    .sendKeys("standard_user");
 
-        System.out.println("SauceDemo login successful");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
 
-        driver.switchTo().newWindow(WindowType.TAB);
+            System.out.println("SauceDemo login successful");
 
-        driver.get("https://automationexercise.com/products");
+            // ---------- Test 2 ----------
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://automationexercise.com/products");
 
-        driver.findElement(By.id("search_product")).sendKeys("Men Tshirt");
-        driver.findElement(By.id("submit_search")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search_product")))
+                    .sendKeys("Men Tshirt");
 
-        WebElement product = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(
-                        By.cssSelector("a[data-product-id='2']")
-                )
-        );
+            driver.findElement(By.id("submit_search")).click();
 
-        ((org.openqa.selenium.JavascriptExecutor) driver)
-                .executeScript("arguments[0].scrollIntoView({block:'center'});", product);
+            WebElement product = wait.until(
+                    ExpectedConditions.elementToBeClickable(
+                            By.cssSelector("a[data-product-id='2']")
+                    )
+            );
 
-        ((org.openqa.selenium.JavascriptExecutor) driver)
-                .executeScript("arguments[0].click();", product);
+            product.click();
 
-        WebElement viewCart = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.cssSelector("#cartModal a[href='/view_cart']")
-                )
-        );
+            WebElement viewCart = wait.until(
+                    ExpectedConditions.elementToBeClickable(
+                            By.cssSelector("#cartModal a[href='/view_cart']")
+                    )
+            );
 
-        ((org.openqa.selenium.JavascriptExecutor) driver)
-                .executeScript("arguments[0].scrollIntoView(true);", viewCart);
+            viewCart.click();
 
-        ((org.openqa.selenium.JavascriptExecutor) driver)
-                .executeScript("arguments[0].click();", viewCart);
+            System.out.println("Automation Exercise product added to cart");
 
-        System.out.println("Automation Exercise product added to cart");
+            // ---------- Test 3 ----------
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://practicetestautomation.com/practice-test-login/");
 
-        driver.switchTo().newWindow(WindowType.TAB);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")))
+                    .sendKeys("student");
 
-        driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.findElement(By.id("password")).sendKeys("Password123");
+            driver.findElement(By.id("submit")).click();
 
-        Thread.sleep(2000);
+            System.out.println("Practice Test Automation login successful");
 
-        driver.findElement(By.id("username")).sendKeys("student");
-        driver.findElement(By.id("password")).sendKeys("Password123");
-        driver.findElement(By.id("submit")).click();
-
-        System.out.println("Practice Test Automation login successful");
-
-        Thread.sleep(5000);
-
-        driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
